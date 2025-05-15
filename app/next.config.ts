0622219b2d31a0dd6e1349bc4fc4afb1adfb5678
WheatChain <wheat.sol.eco@gmail.com>
2025-05-15 15:46:33 +0100

    feat: Configure ESLint and TypeScript settings in next.config.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allow all domains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
